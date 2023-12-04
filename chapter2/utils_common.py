12e2c56f637844688159060f427adc34457c582b
skywalkeryin <skywalkeryin007@gmail.com>
2023-12-05 00:24:04 +0800

    feat: add decision boundary

---
+++ chapter2/utils_common.py
@@ -0,0 +1,51 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from ipywidgets import Output
+
+
+dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0')
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0'
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+
+def draw_vthresh(ax,x):
+    """ draws a threshold """
+    ylim = ax.get_ylim()
+    xlim = ax.get_xlim()
+    ax.fill_between([xlim[0], x], [ylim[1], ylim[1]], alpha=0.2, color=dlblue)
+    ax.fill_between([x, xlim[1]], [ylim[1], ylim[1]], alpha=0.2, color=dldarkred)
+    ax.annotate("z >= 0", xy= [x,0.5], xycoords='data',
+                xytext=[30,5],textcoords='offset points')
+    d = FancyArrowPatch(
+        posA=(x, 0.5), posB=(x+3, 0.5), color=dldarkred,
+        arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
+    )
+    ax.add_artist(d)
+    ax.annotate("z < 0", xy= [x,0.5], xycoords='data',
+                 xytext=[-50,5],textcoords='offset points', ha='left')
+    f = FancyArrowPatch(
+        posA=(x, 0.5), posB=(x-3, 0.5), color=dlblue,
+        arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
+    )
+    ax.add_artist(f)
+
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Parameters
+    ----------
+    z : array_like
+        A scalar or numpy array of any size.
+
+    Returns
+    -------
+     g : array_like
+         sigmoid(z)
+    """
+    z = np.clip( z, -500, 500 )           # protect against overflow
+    g = 1.0/(1.0+np.exp(-z))
+
+    return g
