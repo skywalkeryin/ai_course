bf2828df08bd127255c418fed1f34987c9e15340
skywalkeryin <skywalkeryin007@gmail.com>
2023-12-10 23:10:07 +0800

    feat: implementing  gradient descent for logistic

--- chapter2/utils_common.py
+++ chapter2/utils_common.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import FancyArrowPatch
 from ipywidgets import Output
 
+from matplotlib import cm
+import matplotlib.colors as colors
 
 dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0')
 dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0'
@@ -45,7 +47,94 @@ def sigmoid(z):
      g : array_like
          sigmoid(z)
     """
-    z = np.clip( z, -500, 500 )           # protect against overflow
+    z = np.clip(z, -500, 500 )           # protect against overflow
     g = 1.0/(1.0+np.exp(-z))
 
     return g
+
+
+def compute_cost_logistic(X, y, w, b):
+    """
+    Compute cost
+
+    Args:
+        X (ndarray (m, n)): Data, m examples with n features 
+        y (ndarray (m,))  : Labels, m examplesSS
+        w (ndarray (n,))  : model parameters
+        b (scalar)        : model parameter
+    Returns:
+        cost (scalar)     : cost for logistic regression
+    """
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = X[i]@w + b
+        f_wb_i = sigmoid(z_i)
+        cost += (-y[i]*np.log(f_wb_i) - (1 - y[i]) * np.log(1 - f_wb_i))
+    cost = cost / m
+    return cost
+
+
+# Plot Data
+def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
+    """ plots logistic data with two axis """
+    # Find Indices of Positive and Negative Examples
+    pos = y == 1
+    neg = y == 0
+    pos = pos.reshape(-1,)  #work with 1D or 1D y vectors,  flat the arrays
+    neg = neg.reshape(-1,)
+
+    # Plot examples
+    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c = 'red', label=pos_label)
+    ax.scatter(X[neg, 0], X[neg, 1], marker='o', s=s, label=neg_label, facecolors='none', edgecolors='blue', lw=3)
+    ax.legend(loc=loc)
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
+
+def plt_tumor_data(x, y, ax):
+    """ plots tumor data on one axis """
+    pos = y == 1
+    neg = y == 0
+
+    ax.scatter(x[pos], y[pos], marker='x', s=80, c = 'red', label="malignant")
+    ax.scatter(x[neg], y[neg], marker='o', s=100, label="benign", facecolors='none', edgecolors=dlblue,lw=3)
+    ax.set_ylim(-0.175,1.1)
+    ax.set_ylabel('y')
+    ax.set_xlabel('Tumor Size')
+    ax.set_title("Logistic Regression on Categorical Data")
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
+
+
+    
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    """ truncates color map """
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
+
+def plt_prob(ax, w_out,b_out):
+    """ plots a decision boundary but include shading to indicate the probability """
+    #setup useful ranges and common linspaces
+    x0_space  = np.linspace(0, 4 , 100)
+    x1_space  = np.linspace(0, 4 , 100)
+
+    # get probability for x0,x1 ranges
+    tmp_x0,tmp_x1 = np.meshgrid(x0_space,x1_space)
+    z = np.zeros_like(tmp_x0)
+    for i in range(tmp_x0.shape[0]):
+        for j in range(tmp_x1.shape[1]):
+            z[i,j] = sigmoid(np.dot(w_out, np.array([tmp_x0[i,j],tmp_x1[i,j]])) + b_out)
+
+
+    cmap = plt.get_cmap('Blues')
+    new_cmap = truncate_colormap(cmap, 0.0, 0.5)
+    pcm = ax.pcolormesh(tmp_x0, tmp_x1, z,
+                   norm=cm.colors.Normalize(vmin=0, vmax=1),
+                   cmap=new_cmap, shading='nearest', alpha = 0.9)
+    ax.figure.colorbar(pcm, ax=ax)
